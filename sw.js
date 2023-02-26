fe9fc5e54cbb126f6880766f20c4ad0e5ac9f42c
RL <rlyon14@yahoo.com>
2023-02-26 09:34:13 -0700

    .

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20230226.091754';
+const cacheName = 'chirpy-20230226.093320';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
